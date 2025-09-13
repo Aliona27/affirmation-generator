70d8c2a4d934340bd767c36bd9028bb1eb807231
Aliona27 <alona.lia97@gmail.com>
2025-09-13 12:48:34 +0100

    Added basic form handling and added Typewriter plugin

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+function generateAffirmation(event) {
+  event.preventDefault();
+  new Typewriter("#affirmation", {
+    strings: "Generating your affirmation...✨",
+    autoStart: true,
+    delay: 1,
+    cursor: null,
+  });
+}
 
+let affirmationFormElement = document.querySelector(
+  "#affirmation-generator-form"
+);
+affirmationFormElement.addEventListener("submit", generateAffirmation);
