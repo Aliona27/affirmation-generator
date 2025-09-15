41a12c8f36498583ed81276b92ec87592a232064
Aliona27 <alona.lia97@gmail.com>
2025-09-15 02:21:26 +0100

    Improved the User Experience

--- src/app.js
+++ src/app.js
@@ -13,14 +13,15 @@ function displayAffirmation(response) {
 
 function generateAffirmation(event) {
   event.preventDefault();
-  let affirmationElement = document.querySelector("#affirmation");
-  affirmationElement.innerHTML = "Generating your affirmation...🪄✨";
   let apiKey = "fbaa14d04fa37f863bcc9t0cb9a68a1o";
   let userInput = document.querySelector("#user-input");
   let prompt = `Create an affirmation inspired by the word: ${userInput.value}. Add a relevant emoji at the end.`;
   let context =
     "You are an affirmation generator. Your only task is to create positive, motivational, or soulful affirmation. Each affirmation should feel uplifting, encouraging, and emotionally resonant. Keep your responses short and concise at 1-2 sentences long. Always write in a warm and inspiring tone. Do not provide explanations or lists — only the affirmation itself.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let affirmationElement = document.querySelector("#affirmation");
+  userInput.value = userInput.value.trim();
+  affirmationElement.innerHTML = `Generating your affirmation about ${userInput.value.toLowerCase()}🪄<span class="blink">✨</span>`;
   axios.get(apiUrl).then(displayAffirmation);
 }
 
