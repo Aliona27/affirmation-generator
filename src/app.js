03a73d382e240112812fb81d0c3e053c5dede757
Aliona27 <alona.lia97@gmail.com>
2025-09-15 03:53:43 +0100

    Injected additional JS-functionalities

--- src/app.js
+++ src/app.js
@@ -1,10 +1,6 @@
 function displayAffirmation(response) {
-  let affirmation = response.data.answer;
-  let affirmationElement = document.querySelector("#affirmation");
-  affirmationElement.innerHTML = `${affirmation}`;
-
   new Typewriter("#affirmation", {
-    strings: `${affirmation}💜`,
+    strings: `${response.data.answer}💜`,
     autoStart: true,
     delay: 15,
     cursor: "",
@@ -19,11 +15,21 @@ function generateAffirmation(event) {
   let context =
     "You are an affirmation generator. Your only task is to create positive, motivational, or soulful affirmation. Each affirmation should feel uplifting, encouraging, and emotionally resonant. Keep your responses short and concise at 1-2 sentences long. Always write in a warm and inspiring tone. Do not provide explanations or lists — only the affirmation itself.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  let affirmationElement = document.querySelector("#affirmation");
   userInput.value = userInput.value.trim();
   affirmationElement.innerHTML = `Generating your affirmation about ${userInput.value.toLowerCase()}🪄<span class="blink">✨</span>`;
   axios.get(apiUrl).then(displayAffirmation);
 }
+
+let affirmationElement = document.querySelector("#affirmation");
+let affirmation = `You are exactly where you need to be!💫 <br />
+          Trust the process of life and enjoy your unique path💜`;
+affirmationElement.innerHTML = affirmation;
+new Typewriter("#affirmation", {
+  strings: affirmation,
+  autoStart: true,
+  delay: 15,
+  cursor: "",
+});
 
 let affirmationFormElement = document.querySelector(
   "#affirmation-generator-form"
